42c39e9553f6a387857eac4b0604f077830ed0c9
Douglas Williams <douglaswilliams298@gmail.com>
2022-01-07 16:30:36 -0300

    Multiplicação sem utilizar *

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Oi, eu sou um script')
+function multiplicarDoisNumeros(numero1, numero2) {
+    let soma = 0
+    for (let index = 0; index < numero1; index++) {
+        soma = soma + numero2 // soma += numero2
+    }
+        // return soma
+        console.log(soma)
+}
+    //console.log(multiplicarDoisNumeros(3, 4))
+
+    //console.log(multiplicarDoisNumeros(6, 5))
